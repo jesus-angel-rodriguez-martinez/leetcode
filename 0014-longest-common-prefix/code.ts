12d62503b636f035d2b41ce3c685099e5af67625
Jesús Ángel Rodríguez Martínez <sudo.von.contact@gmail.com>
2026-01-22 07:28:16 -0600

    feat: :sparkles: add 0014-longest-common-prefix

---
+++ 0014-longest-common-prefix/code.ts
@@ -0,0 +1,12 @@
+function longestCommonPrefix(strs: string[]): string {
+  let result = "";
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 0; j < strs.length; j++) {
+      if (i === strs[j].length || strs[0][i] !== strs[j][i]) {
+        return result;
+      }
+    }
+    result += strs[0][i];
+  }
+  return result;
+}
