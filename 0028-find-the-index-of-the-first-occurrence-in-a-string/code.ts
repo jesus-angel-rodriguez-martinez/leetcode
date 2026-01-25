088eb66080426734c95f1559b855f6620f99ff93
Jesús Ángel Rodríguez Martínez <sudo.von.contact@gmail.com>
2026-01-24 22:59:29 -0600

    feat: :sparkles: add 0028-find-the-index-of-the-first-occurrence-in-a-string

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/code.ts
@@ -0,0 +1,11 @@
+function strStr(haystack: string, needle: string): number {
+  if (!needle) {
+    return 0;
+  }
+  for (let i = 0; i <= haystack.length - needle.length; i++) {
+    if (haystack.startsWith(needle, i)) {
+      return i;
+    }
+  }
+  return -1;
+}
