536689d5ce5d4f6f104f2c8dac171e96360dd521
Jesús Ángel Rodríguez Martínez <sudo.von.contact@gmail.com>
2026-02-11 23:12:59 -0600

    feat: :sparkles: add 160-intersection-of-two-linked-lists

---
+++ 160-intersection-of-two-linked-lists/code.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function getIntersectionNode(
+  headA: ListNode | null,
+  headB: ListNode | null,
+): ListNode | null {
+  let currentA = headA;
+  let currentB = headB;
+
+  while (currentA !== currentB) {
+    currentA = currentA ? currentA.next : headB;
+    currentB = currentB ? currentB.next : headA;
+  }
+
+  return currentA;
+}
