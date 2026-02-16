6697d4cc29e1d37f270b105bff95bb70a820d6dd
Jesús Ángel Rodríguez Martínez <sudo.von.contact@gmail.com>
2026-02-15 21:15:57 -0600

    feat: :sparkles: add 1290-convert-binary-number-in-a-linked-list-to-integer

---
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/code.ts
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function getDecimalValue(head: ListNode | null): number {
+  let total = 0;
+  let current = head;
+  while (current) {
+    total = total * 2 + current.val;
+    current = current.next;
+  }
+  return total;
+}
