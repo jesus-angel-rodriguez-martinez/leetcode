ce623b2c102c1cc2350caef48003c5ee5585492e
Jesús Ángel Rodríguez Martínez <sudo.von.contact@gmail.com>
2026-02-21 00:41:18 -0600

    feat: :sparkles: add 2095-delete-the-middle-node-of-a-linked-list

---
+++ 2095-delete-the-middle-node-of-a-linked-list/code.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function deleteMiddle(head: ListNode | null): ListNode | null {
+  if (!head) {
+    return null;
+  }
+
+  let slow = head;
+  let fast = head;
+  let previous = null;
+
+  while (fast && fast.next) {
+    previous = slow;
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  previous.next = slow.next;
+
+  return head;
+}
