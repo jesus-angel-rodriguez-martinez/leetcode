8d18d53ea1b8646e0047ad297a8201bed388d010
Jesús Ángel Rodríguez Martínez <sudo.von.contact@gmail.com>
2026-02-23 05:12:54 -0600

    feat: :sparkles: add 0328-odd-even-linked-list

---
+++ 0328-odd-even-linked-list/code.ts
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function oddEvenList(head: ListNode | null): ListNode | null {
+  if (!head || !head.next) {
+    return head;
+  }
+
+  let odd = head;
+  let even = head.next;
+
+  const evenHead = head.next;
+
+  while (even && even.next) {
+    odd.next = odd.next.next;
+    odd = odd.next;
+
+    even.next = even.next.next;
+    even = even.next;
+  }
+
+  odd.next = evenHead;
+
+  return head;
+}
