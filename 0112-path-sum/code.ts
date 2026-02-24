416cf1c5d96614e459e6ea2c67efa2496beec8b5
Jesús Ángel Rodríguez Martínez <sudo.von.contact@gmail.com>
2026-02-24 00:05:55 -0600

    feat: :sparkles: add 0112-path-sum

---
+++ 0112-path-sum/code.ts
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
+  const dfs = (node: TreeNode | null, currentSum: number) => {
+    if (!node) {
+      return false;
+    }
+
+    currentSum += node.val;
+
+    if (!node.left && !node.right) {
+      return currentSum === targetSum;
+    }
+
+    return dfs(node.left, currentSum) || dfs(node.right, currentSum);
+  };
+  return dfs(root, 0);
+}
