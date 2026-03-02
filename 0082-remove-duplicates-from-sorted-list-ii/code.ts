c7c2b8abe97ba05447f447402dc11c26da896d5d
Jesús Ángel Rodríguez Martínez <sudo.von.contact@gmail.com>
2026-03-02 02:45:07 -0600

    feat: :sparkles: add 0082-remove-duplicates-from-sorted-list-ii

---
+++ 0082-remove-duplicates-from-sorted-list-ii/code.ts
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+  const dummy = new ListNode(0, head);
+
+  let previous = dummy;
+  while (head) {
+    if (head.next && head.val === head.next.val) {
+      while (head.next && head.val === head.next.val) {
+        head = head.next;
+      }
+      previous.next = head.next;
+    } else {
+      previous = previous.next;
+    }
+    head = head.next;
+  }
+
+  return dummy.next;
+}
