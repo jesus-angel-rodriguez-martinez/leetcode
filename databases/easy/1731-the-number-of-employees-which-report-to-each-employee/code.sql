21db49360971def379c4a145a40dcbd62237ca35
Jesús Ángel Rodríguez Martínez <jesus.arm.dev@gmail.com>
2026-07-18 00:15:34 -0700

    feat: :sparkles: add 1731-the-number-of-employees-which-report-to-each-employee

---
+++ databases/easy/1731-the-number-of-employees-which-report-to-each-employee/code.sql
@@ -0,0 +1,16 @@
+SELECT
+    e2.employee_id AS employee_id,
+    e2.name AS name,
+    COUNT(*) AS reports_count,
+    ROUND(AVG(e1.age)) AS average_age
+FROM
+    Employees AS e1
+INNER JOIN
+    Employees AS e2
+ON
+    e1.reports_to = e2.employee_id
+GROUP BY
+    e2.employee_id,
+    e2.name
+ORDER BY
+    e2.employee_id ASC;
