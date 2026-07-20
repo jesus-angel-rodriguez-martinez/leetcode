4859138bfedbe77a4b67bd790c2149f3d848b40b
Jesús Ángel Rodríguez Martínez <jesus.arm.dev@gmail.com>
2026-07-19 17:41:10 -0700

    feat: :sparkles: add 1873-calculate-special-bonus

---
+++ databases/easy/1873-calculate-special-bonus/code.sql
@@ -0,0 +1,12 @@
+SELECT
+    e.employee_id AS employee_id,
+    CASE
+        WHEN
+            e.employee_id % 2 = 1
+        AND
+            LEFT(e.name, 1) <> 'M'
+        THEN e.salary
+        ELSE 0
+    END AS bonus
+FROM
+    Employees AS e;
